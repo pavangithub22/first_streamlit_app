b5843e2af3f6f7ceeb7585ea3e455abff9b88782
Pavan Aluvala <108463273+pavangithub22@users.noreply.github.com>
2022-07-14 16:05:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
 
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇'
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
