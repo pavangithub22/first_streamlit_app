7b47590f04f8387b9fc7f28fa950a4c371c09df9
Pavan Aluvala <108463273+pavangithub22@users.noreply.github.com>
2022-07-14 17:03:16 +0530

    Update streamlit_app.py
    added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,18 @@ import streamlit
 
 streamlit.title('My Moms New Healthhy Diner')
 
+streamlit.header('Breakfast Menu')
+
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+
+streamlit.text('🥑🍞Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
 
 
