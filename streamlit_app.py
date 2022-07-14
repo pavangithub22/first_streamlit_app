5f00629c6226f5f4993a9b023cf6452db472f7ab
Pavan Aluvala <108463273+pavangithub22@users.noreply.github.com>
2022-07-14 15:47:23 +0530

    Update streamlit_app.py
    added diner items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.title('Breakfast Menu')
+
+Omega 3 & Blueberry Oatmeal
+
+Kale, Spinach & Rocket Smoothie
+
+Hard-Boiled Free-Range Egg
